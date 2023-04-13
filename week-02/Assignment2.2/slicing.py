a6958ceae8f6619f07eb05e0e788d52184b6fe2e
Mehboob Ali <118427945+mebi-ali@users.noreply.github.com>
2023-04-13 13:33:46 +0500

    Adding comments

--- week-02/Assignment2.2/slicing.py
+++ week-02/Assignment2.2/slicing.py
@@ -1,15 +1,18 @@
-# %%
-
 import numpy as np
 
+# Create a 6x4 matrix with random integer values between 20 and 85
 matrix = np.random.randint(low=20, high=85, size=(6,4))
 
+# Print the initial matrix
 print(f'Inital Matrix: \n\n {matrix}')
 
+# Add a new row at index 4 that is the sum of rows 0 and 2
 matrix[4] = np.sum( matrix[[0,2], :], axis=0)
+
+# Add a new row at index 5 that is the sum of rows 1 and 3
 matrix[5] = np.sum( matrix[[1,3], :], axis=0)
 
+# Print the modified matrix
 print(f'\nMatrix After Slicing: \n\n {matrix}')
-
 
 
